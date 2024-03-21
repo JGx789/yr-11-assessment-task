62788d79d0078995f928352711c33a528ddd2ab5
JGx789 <162063175+JGx789@users.noreply.github.com>
2024-03-22 10:06:39 +1100

    started functions

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
+def TestForEqual(S1,S2,S3):# test for equilateral triangle
+  if S1==S2==S3:
+    print("it is a Equilateral Triangle")
+  else:
+    print("it is NOT a Equilateral Triangle") 
+ 
+def TestForIsos(S1,S2,S3):# test for isosceles triangle
+  if S1==S2!=S3 or S2==S3!=S1 or S1==S3!=S2:
+    print("it is a Isosceles Triangle")
+  else:
+    print("it is NOT a Isosceles Triangle")
+
+def TestforScal(S1,S2,S3):# test for scalene triangle
+   if S1!=S2!=S3:
+    print("it is a Scalene Triangle")
+   else:
+    print("it is NOT a Scalene Triangle")
+
+
+ # start of main program
 Side1=input("Enter Side1 Length ")
 Side2=input("Enter Side2 Length ")
 Side3=input("Enter Side3 Length ")
-if Side1==Side2==Side3:
-    print("it is a Equilateral Triangle")
-else:
-    print("it is NOT a Equilateral Triangle")
-if Side1==Side2 or Side2==Side3 or Side1==Side3:
-    print("it is a Isosceles Triangle")
-else:
-    print("it is NOT a Isosceles Triangle")
-if Side1!=Side2!=Side3:
-    print("it is a Scalene Triangle")
-else:
-    print("if is NOT a Scalene Triangle")
+
+TestForEqual(Side1,Side2,Side3)# this is the def test for equailateral
+
+TestForIsos(Side1,Side2,Side3)
+
+TestforScal(Side1,Side2,Side3)
